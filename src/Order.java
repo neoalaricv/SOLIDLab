f3117098d1e3184a2e706ec22e68e6c1c087f23c
Neo Villanueva <142380105+neoalaricv@users.noreply.github.com>
2024-05-18 11:55:16 +0800

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,29 +1,3 @@
-public class Order {
-    private TotalCalculator totalCalculator;
-    private OrderPlacer orderPlacer;
-    private InvoiceGenerator invoiceGenerator;
-    private EmailNotifier emailNotifier;
-
-    public Order(TotalCalculator totalCalculator, OrderPlacer orderPlacer, InvoiceGenerator invoiceGenerator, EmailNotifier emailNotifier) {
-        this.totalCalculator = totalCalculator;
-        this.orderPlacer = orderPlacer;
-        this.invoiceGenerator = invoiceGenerator;
-        this.emailNotifier = emailNotifier;
-    }
-
-    public void calculateTotal(double price, int quantity) {
-        totalCalculator.calculateTotal(price, quantity);
-    }
-
-    public void placeOrder(String customerName, String address) {
-        orderPlacer.placeOrder(customerName, address);
-    }
-
-    public void generateInvoice(String fileName) {
-        invoiceGenerator.generateInvoice(fileName);
-    }
-
-    public void sendEmailNotification(String email) {
-        emailNotifier.sendEmailNotification(email);
-    }
+public interface Order {
+    void processOrder(OrderInfo orderInfo);
 }
