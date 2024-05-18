287dd348eb8f1807073ea8bba1e69a55acfda7bd
Neo Villanueva <142380105+neoalaricv@users.noreply.github.com>
2024-05-18 11:54:14 +0800

    Update InvoiceGenerator.java

--- src/InvoiceGenerator.java
+++ src/InvoiceGenerator.java
@@ -1,3 +1,5 @@
-public interface InvoiceGenerator {
-    void generateInvoice(String fileName);
+public class InvoiceGenerator {
+    public void generateInvoice(String fileName) {
+        System.out.println("Invoice generated: " + fileName);
+    }
 }
