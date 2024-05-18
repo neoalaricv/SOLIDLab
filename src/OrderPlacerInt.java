386dfed9a27f36d1a05e3b986ec189bd2fae83d7
Neo Villanueva <142380105+neoalaricv@users.noreply.github.com>
2024-05-18 11:57:57 +0800

    Delete src/OrderPlacerInt.java

--- src/OrderPlacerInt.java
+++
@@ -1,7 +0,0 @@
-public class OrderPlacerInt implements OrderPlacer {
-
-    @Override
-    public void placeOrder(String customerName, String address) {
-        System.out.println("Order placed for " + customerName + " at " + address);
-    }
-}
